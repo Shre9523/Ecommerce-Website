392c45e71223a3c6643d4a37bc14ee1dd16df260
Shreyansh kumar singh <98299601+Shre9523@users.noreply.github.com>
2022-08-02 17:26:51 +0530

    Add files via upload

---
+++ Ecommerce Website/Ecommerce/Ecommerce.js
@@ -0,0 +1,20 @@
+var menuItems = document.getElementById("MenuItems");
+
+menuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+
+    if(menuItems.style.maxHeight == "0px"){
+   
+        menuItems.style.maxHeight = "200px"
+
+
+    }else{
+
+        menuItems.style.maxHeight = "0px"
+    }
+
+
+
+
+}
